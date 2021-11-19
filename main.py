a6ab4bf0775eaad2f0d640262e50611fa5ac5d30
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-11-19 09:10:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+#!/usr/bin/env python
+
 import json 
 
 json_letters = """
-{
-    "a" : {
-        "days" : "34"
-        }
-}
+    {
+        "a" : "1",
+        "h" : "2",
+        "o" : "3", 
+        "j" : "4"
+    }
 """
 
 phrase = input("Enter a phrase: ")
 
 parsed_letters = json.loads(json_letters)
 
-print(parsed_letters["a"])
+def main():
+    days = 0
+
+    for letter in phrase:
+        days += int(parsed_letters[letter])
+
+    print(days)
+
+main()
